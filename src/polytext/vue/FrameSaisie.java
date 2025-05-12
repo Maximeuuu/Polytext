cbbc47ebd6ada40116ba4f846f0e1f1abd809d7d
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2025-05-13 00:13:47 +0200

    ajout du panel statistique en dessous

--- src/polytext/vue/FrameSaisie.java
+++ src/polytext/vue/FrameSaisie.java
@@ -1,7 +1,7 @@
 
 package polytext.vue;
 
-import java.awt.MenuBar;
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
@@ -13,7 +13,9 @@ public class FrameSaisie extends JFrame
 	{
 		super.setTitle( "Editeur" );
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		super.add( new PanelSaisie( ctrl ) );
+		super.setLayout( new BorderLayout() );
+		super.add( new PanelSaisie( ctrl ), BorderLayout.CENTER );
+		super.add( new PanelStatistiques(ctrl), BorderLayout.SOUTH );
 		super.setSize(1500, 700);
 		super.setLocationRelativeTo( null );
 		super.setJMenuBar( new MenuPolytext(ctrl) );
